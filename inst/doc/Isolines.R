e43deb64d75cee8856f31c4d92b9fa5aba8eb605
Jens von Bergmann <jens@mountainmath.ca>
2024-04-18 16:12:35 +0000

    version 0.1.8

--- inst/doc/Isolines.R
+++ inst/doc/Isolines.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -10,7 +10,7 @@ library(ggplot2)
 library(VancouvR)
 
 ## -----------------------------------------------------------------------------
-ggplot(get_cov_data("elevation-contour-lines-1-metre-contours",format="geojson") ) + 
+ggplot(get_cov_data("elevation-contour-lines-1-metre-contours") ) + 
   geom_sf(aes(color=elevation),size=0.1) + 
   scale_color_viridis_c(option="inferno",guide=FALSE) + 
   theme_void()
